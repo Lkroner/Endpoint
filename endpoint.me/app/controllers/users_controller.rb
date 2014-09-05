ae969adff58059a5c58b55dd341776992eac4fda
kbasma <katia.bazzi@gmail.com>
2014-09-05 14:56:49 -0700

    added create, show, update actions for users controller

--- endpoint.me/app/controllers/users_controller.rb
+++ endpoint.me/app/controllers/users_controller.rb
@@ -1,6 +1,35 @@
 class UsersController < ApplicationController
+  # POST '/users'
   def create
+    new_user = User.new(params[:user])
+    if new_user.save 
+      render json: {user: new_user}.to_json
+    else
+      render json: {errors: new_user.errors}
+    end
   end
+
+  # GET '/users/:id'
+  def show
+    user = User.find(params[:id])
+    if user
+      render json: {user: user}.to_json
+    else
+      render status: :unprocessable_entity, json: { message: "#{params[:id]} is not a valid api id!" }.to_json
+    end
+  end
+
+
+  # PUT '/users/:id'
+  def update
+    user = User.find(params[:id])
+      if user.update_attributes(params[:user])
+        render json: {user: user}.to_json
+      else
+        render status: :unprocessable_entity, json: {errors: user.errors}.to_json
+      end
+  end
+
 
   def login
   end
@@ -12,12 +41,5 @@ class UsersController < ApplicationController
   end
 
   def github_callback
-
   end
-
-private
-
-def user_params
-end
-
 end
