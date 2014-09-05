b7e8a9010a74e9c60b73c1f7fe1b5af7341510cf
Nina Church <ninapearlchurch@gmail.com>
2014-09-04 21:25:01 -0700

    added all mvp resources

--- endpoint.me/config/routes.rb
+++ endpoint.me/config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+  root 'search#index'
+
+  resources :users, only: [:create, :show, :update] do
+    resources :reviews, only: [:create, :index]
+    resources :comments, only: [:create, :index]
+  end
+
+  resources :apis, only: [:create, :show, :update] do
+    resources :reviews, only: [:create, :index] do
+      resources :votes, only: [:create, :index]
+      resources :comments, only: [:create, :index]
+    end
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
