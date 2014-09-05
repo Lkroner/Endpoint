0ba9ebf4ad1c6943f6fa7fc770093757ce908158
yohanyi <yohanyista@gmail.com>
2014-09-05 11:06:27 -0700

    initial setup for oauth

--- endpoint.me/app/controllers/users_controller.rb
+++ endpoint.me/app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def create
   end
+
+  def login
+  end
+
+  def login_user
+  end
+
+  def logout
+  end
+
+  def github_callback
+    CLIENT_ID
+    CLIENT_SECRET
+
+  end
+
+
 end
