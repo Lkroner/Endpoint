14cd5115e7ea242458c356c354f3a5dbcb867a28
kbasma <katia.bazzi@gmail.com>
2014-09-05 15:56:16 -0700

    added index and create action for the reviews controller

--- endpoint.me/app/controllers/reviews_controller.rb
+++ endpoint.me/app/controllers/reviews_controller.rb
@@ -1,7 +1,27 @@
 class ReviewsController < ApplicationController
+  # GET /apis/:api_id/reviews
+  # GET /users/:user_id/reviews
   def index
+    if params[:api_id]
+    	api = Api.find(:api_id)
+    	reviews = api.reviews
+    	render json: {reviews: reviews}.to_json
+    elsif params[:user_id]
+      	user = User.find(params[:user_id])
+      	reviews = user.reviews 
+      	render json: {reviews: reviews}.to_json
+    else
+      render json: { message: "#{params[:api_id]} or #{params[:user_id]}} is not a valid review/user id!" }.to_json
+    end
   end
 
-  def show
+  # POST /apis/:api_id/reviews
+  def create
+  	new_review = Review.new(params[:review])
+  	if new_review.save 
+  		render json: {review: new_review}.to_json
+  	else
+  		render json: {errors: new_review.errors}
+  	end
   end
 end
