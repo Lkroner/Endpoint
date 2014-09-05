2d67bd69e292d04ecff3ae2ac66f2fccd03bcc84
kbasma <katia.bazzi@gmail.com>
2014-09-05 14:20:07 -0700

    added /search route

--- endpoint.me/config/routes.rb
+++ endpoint.me/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root 'search#index'
+  get 'search' => "search#search"
 
   resources :users, only: [:create, :show, :update] do
     resources :reviews, only: [:create, :index]
