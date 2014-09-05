97e767369b4b1b7cb44491cc2482c59ded881ee7
I love turtles <ninapearlchurch@gmail.com>
2014-09-04 18:24:21 -0700

    Removed White Space

--- endpoint/db/migrate/20140905001507_create_followings.rb
+++ endpoint/db/migrate/20140905001507_create_followings.rb
@@ -1,7 +1,7 @@
 class CreateFollowings < ActiveRecord::Migration
   def change
     create_table :followings do |t|
-  		t.belongs_to :user
+      t.belongs_to :user
       t.belongs_to :api
       t.timestamps
     end
