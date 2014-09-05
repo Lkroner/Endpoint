0b8a8704d7905cb6e370a9b6f20aa580f41b91ed
kbasma <katia.bazzi@gmail.com>
2014-09-04 21:57:38 -0700

    finished seed file

--- endpoint.me/app/models/api.rb
+++ endpoint.me/app/models/api.rb
@@ -1,6 +1,7 @@
 class Api < ActiveRecord::Base
 	has_many :reviews
-	validates :title, :description, :average_rating, presence: true
+	validates :title, :description, :average_score, presence: true
 	has_many :followings
 	has_many :users, through: :followings
+	# attr_accessor :average_score
 end
--- endpoint.me/db/seeds.rb
+++ endpoint.me/db/seeds.rb
@@ -5,29 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-20.times {
-	User.create(email: Faker::Internet.email,
-				password: Faker::Internet.password,
-				about_me: Faker::Lorem.sentence(3),
-				picture_url: 'http://www.chadcreates.com/wp-content/uploads/2009/05/People-128.png'
-				)
-}
+# 20.times {
+# 	User.create(email: Faker::Internet.email,
+# 				password: Faker::Internet.password,
+# 				about_me: Faker::Lorem.sentence(3),
+# 				picture_url: 'http://www.chadcreates.com/wp-content/uploads/2009/05/People-128.png'
+# 				)
+# }
 
 
-10.times{
-	Api.create(title: Faker::Lorem.word,
-			   description: Faker::Lorem.sentence(3),
-			   tips: Faker::Lorem.sentence(3),
-			   average_score: rand(1..5),
-			   logo_url: "http://www.nasa.gov/sites/default/files/sydney.b.boen/images/twitter-small(1).gifv"
-			   )
-}
+# 10.times{
+# 	Api.create(title: Faker::Lorem.word,
+# 			   description: Faker::Lorem.sentence(3),
+# 			   tips: Faker::Lorem.sentence(3),
+# 			   average_score: rand(1..5),
+# 			   logo_url: "http://www.nasa.gov/sites/default/files/sydney.b.boen/images/twitter-small(1).gifv"
+# 			   )
+# }
 
-50.times {
-	review = Review.create(score: rand(1..5),
-				  		   content: Faker::Lorem.sentence(3),
-				           title: Faker::Lorem.word)
+# 50.times {
+# 	review = Review.create(score: rand(1..5),
+# 				  		   content: Faker::Lorem.sentence(3),
+# 				           title: Faker::Lorem.word)
 
-	api = Api.find(rand(1..10))
-	api.reviews << review
-}
+# 	api = Api.find(rand(1..10))
+# 	api.reviews << review
+# }
+
+# 100.times {
+# 	vote = Vote.create()
+# 	user = User.find(rand(1..20))
+# 	review = Review.find(rand(1..50))
+# 	user.votes << vote
+# 	review.votes << vote
+# }
+
+# 100.times {
+# 	comment = Comment.create(content: Faker::Lorem.sentence(3))
+# 	user = User.find(rand(1..20))
+# 	review = Review.find(rand(1..50))
+# 	user.comments << comment
+# 	review.comments << comment
+# }
