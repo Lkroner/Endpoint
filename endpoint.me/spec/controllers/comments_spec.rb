2695f2a1e66ed77f5764ff6c857f3b27f92d906d
kbasma <katia.bazzi@gmail.com>
2014-09-05 18:26:56 -0700

    changed tab size from 4 to 2

--- endpoint.me/spec/controllers/comments_spec.rb
+++ endpoint.me/spec/controllers/comments_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 describe CommentsController do
-		before :each do
-    	User.destroy_all
-      Comment.destroy_all
-      Review.destroy_all
- 		end
-    it "responds successfully with an HTTP 200 status for showing a users' comments" do
-      user = User.create(email: "hi@gmial.com", about_me: "hiiiiii")
-      comment = Comment.create(content: "hiiiii")
-      user.comments << comment
-      get :index, user_id: user.id
-      expect(response).to be_success
-    end
+  before :each do
+    User.destroy_all
+    Comment.destroy_all
+    Review.destroy_all
+  end
+  it "responds successfully with an HTTP 200 status for showing a users' comments" do
+    user = User.create(email: "hi@gmial.com", about_me: "hiiiiii")
+    comment = Comment.create(content: "hiiiii")
+    user.comments << comment
+    get :index, user_id: user.id
+    expect(response).to be_success
+  end
 end
