f460e927d62d65009856007cda31816bbad3a9f0
kbasma <katia.bazzi@gmail.com>
2014-09-06 12:08:41 -0700

    made individual js file for homepage, login, navbar, router, and search results

---
+++ endpoint.me/public/javascripts/searchresults.js
@@ -0,0 +1,9 @@
+app.SearchResults = {
+	Models: {},
+	Collections: {},
+	Views: {}
+}
+
+app.SearchResults.Models.Name1 = Backbone.Model.extend({})
+app.SearchResults.Views.Name2 = Backbone.View.extend({})
+app.SearchResults.Collections.Name3 = Backbone.Collection.extend({})
