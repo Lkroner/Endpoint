f5118b83365489153e8bbc37321872fc3f8b2b0c
kbasma <katia.bazzi@gmail.com>
2014-09-05 18:22:27 -0700

    deleted validations because it is messing with test suite

--- endpoint.me/app/models/api.rb
+++ endpoint.me/app/models/api.rb
@@ -1,6 +1,5 @@
 class Api < ActiveRecord::Base
 	has_many :reviews
-	# validates :title, :description, presence: true
 	has_many :followings
 	has_many :users, through: :followings
 	attr_accessor :average_score
