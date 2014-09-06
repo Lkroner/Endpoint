c9cfebe477b4c9867dd9d0b5d712cfa3ac6d254f
Evan Simoni <evan.simoni@gmail.com>
2014-09-05 22:44:37 -0700

    adds a save test for votes

--- endpoint.me/spec/controllers/votes_spec.rb
+++ endpoint.me/spec/controllers/votes_spec.rb
@@ -12,4 +12,11 @@ describe VotesController do
     expect(response).to be_success
   end
 
+  it "accurately saves a new vote" do
+    review = Review.create(score: 2.5, content:"I had a mediocre experience", title:"it was OK", api_id: 1)
+    vote = Vote.create(user_id: 1, review_id: 1)
+    expect {
+      post :create, review_id: review.id, user_id: 1
+      }.to change{review.votes.count}
+  end
 end
