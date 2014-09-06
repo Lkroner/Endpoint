40bcc1a080dc9e3fdfc757bfcd9015eaae66364b
kbasma <katia.bazzi@gmail.com>
2014-09-05 21:58:37 -0700

    changed spacing

--- endpoint.me/spec/controllers/users_spec.rb
+++ endpoint.me/spec/controllers/users_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 describe UsersController do
-		before :each do
-    	User.destroy_all
- 		end
+    before :each do
+      User.destroy_all
+    end
     it "successfully creates a new user" do
       user = User.create(email: "hi@gmail.com",
                          password: "hiiiiii",
@@ -13,15 +13,15 @@ describe UsersController do
     end
 
     it "responds successfully with an HTTP 200 status code for showing a user" do
-    	user = User.create(email: "hi@gmail.com", password: "hiiiiii", about_me: "i'm a person")
+      user = User.create(email: "hi@gmail.com", password: "hiiiiii", about_me: "i'm a person")
       get :show, id: user.id
       expect(response).to be_success
     end
 
     it "successfully updates a user" do
-    	user = User.create(email: "hi@gmail.com", password: "hiiiiii", about_me: "i'm a person")
-    	expect {
-    		user.update_attributes(email: 'meep')
-    	}.to change {user.email}
+      user = User.create(email: "hi@gmail.com", password: "hiiiiii", about_me: "i'm a person")
+      expect {
+        user.update_attributes(email: 'meep')
+      }.to change {user.email}
     end
 end
