5447f53ffb3d776567966b1d00b42f3e58829c7b
kbasma <katia.bazzi@gmail.com>
2014-09-05 17:11:06 -0700

    added controller tests for search

--- endpoint.me/spec/controllers/search_spec.rb
+++ endpoint.me/spec/controllers/search_spec.rb
@@ -1,1 +1,10 @@
-search_spec.rb
+require 'rails_helper'
+describe SearchController do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+end
