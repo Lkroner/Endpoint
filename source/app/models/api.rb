b8931360d15aaa2d4a47ad84e01b5bc564773dee
kbasma <katia.bazzi@gmail.com>
2014-09-06 19:39:34 -0700

    deleted attr_accessor comment in api model

--- source/app/models/api.rb
+++ source/app/models/api.rb
@@ -4,5 +4,4 @@ class Api < ActiveRecord::Base
 	has_many :users, through: :followings
 	include PgSearch					
 	multisearchable :against => [:title, :description]
-	# attr_accessor :average_score
 end
