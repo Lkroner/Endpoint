49282b5c5cab0ecb267a7d95efb6640c57d19c21
Evan Simoni & Philippine FancyPants Crawfish <pair+evansimoni.phillipcrawford@devbootcamp.com>
2014-09-06 20:54:17 -0700

    sets up skeleton for reviews model and view

--- source/public/javascripts/apiprofilepage.js
+++ source/public/javascripts/apiprofilepage.js
@@ -45,3 +45,41 @@ app.ApiProfile.Views.Profile = Backbone.View.extend({
     return this;
   },
 })
+
+/////////////// Reviews Section
+app.ApiProfile.Models.Reviews = Backbone.Model.extend({
+  // do we intialize our model here?
+  // we will pass the model into the view
+  initialize: function(){
+  },
+
+  defaults: {
+    "title": "Google",
+    "description": "This is the Google Api This is the Google Api This is the Google ApiThis is the Google ApiThis is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api This is the Google Api",
+    "tips": "Read the docs. RTFM.",
+    "average_score": "3.4",
+    "logo_url": "http://www.efytimes.com/admin/useradmin/photo/google-mobile-app-store-logo.png",
+    "data_type": "JSON",
+    "doc_link": "www.google.com",
+    "endpoint_link": "www.google.com/endpoints",
+    "num_followers": 302
+  }
+
+})
+
+app.ApiProfile.Views.Reviews = Backbone.View.extend({
+
+  // do an ajax call to get the reviews n shit we need
+  model: new app.ApiProfile.Models.Reviews,
+
+  initialize: function(){
+  },
+
+  template: _.template($('#apiprofile-template').html()),
+
+  render2: function() {
+    console.log('render2 called')
+    this.$el.html(this.template(this.model.attributes));
+    return this;
+  },
+})
