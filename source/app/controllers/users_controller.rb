9ef8807a21d069824dbd8760697519e6d06e7599
kbasma <katia.bazzi@gmail.com>
2014-09-07 21:35:35 -0700

    fixed spacing

--- source/app/controllers/users_controller.rb
+++ source/app/controllers/users_controller.rb
@@ -1,56 +1,56 @@
 class UsersController < ApplicationController
-	# POST '/users'
-	def create
-		@user = User.new(email: params[:email])
-		@user.password = params[:password]
-		if @user.save
-			render json: {user: @user}.to_json
-		else
-			errors = []
-			@user.errors.messages.each do |property, message|
-				errors.concat(message)
-			end
-			render json: {errors: errors}
-		end
-	end
+  # POST '/users'
+  def create
+    @user = User.new(email: params[:email])
+    @user.password = params[:password]
+    if @user.save
+      render json: {user: @user}.to_json
+    else
+      errors = []
+      @user.errors.messages.each do |property, message|
+        errors.concat(message)
+      end
+      render json: {errors: errors}
+    end
+  end
 
-	# GET '/users/:id'
-	def show
-		user = User.find(params[:id])
-		if user
-			render json: {user: user}.to_json
-		else
-			render status: :unprocessable_entity, json: { message: "#{params[:id]} is not a valid api id!" }.to_json
-		end
-	end
-
-
-	# PUT '/users/:id'
-	def update
-		user = User.find(params[:id])
-			if user.update_attributes(params[:user])
-				render json: {user: user}.to_json
-			else
-				render status: :unprocessable_entity, json: {errors: user.errors}.to_json
-			end
-	end
+  # GET '/users/:id'
+  def show
+    user = User.find(params[:id])
+    if user
+      render json: {user: user}.to_json
+    else
+      render status: :unprocessable_entity, json: { message: "#{params[:id]} is not a valid api id!" }.to_json
+    end
+  end
 
 
-	def login
-		user = User.where("email = ?", params[:email]).first
-		if user && user.password == params[:password]
-			render json: {user: user}.to_json
-		else
-			render json: {message: "user doesn't exist"}.to_json
-		end
-	end
+  # PUT '/users/:id'
+  def update
+    user = User.find(params[:id])
+      if user.update_attributes(params[:user])
+        render json: {user: user}.to_json
+      else
+        render status: :unprocessable_entity, json: {errors: user.errors}.to_json
+      end
+  end
 
-	def login_user
-	end
 
-	def logout
-	end
+  def login
+    user = User.where("email = ?", params[:email]).first
+    if user && user.password == params[:password]
+      render json: {user: user}.to_json
+    else
+      render json: {message: "user doesn't exist"}.to_json
+    end
+  end
 
-	def github_callback
-	end
+  def login_user
+  end
+
+  def logout
+  end
+
+  def github_callback
+  end
 end
