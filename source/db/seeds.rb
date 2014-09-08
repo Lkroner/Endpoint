1870e88b90b96f825e068db4d139be2db49840db
Evan Simoni <evan.simoni@gmail.com>
2014-09-08 15:26:10 -0700

    grabs a single object from programmable web in the seed file

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,46 +1,57 @@
-20.times {
-	user = User.new(email: Faker::Internet.email,
-					about_me: Faker::Lorem.sentence(3),
-					picture_url: 'http://www.chadcreates.com/wp-content/uploads/2009/05/People-128.png'
-					)
-	user.password = Faker::Internet.password
-	user.save
-}
+# 20.times {
+# 	user = User.new(email: Faker::Internet.email,
+# 					about_me: Faker::Lorem.sentence(3),
+# 					picture_url: 'http://www.chadcreates.com/wp-content/uploads/2009/05/People-128.png'
+# 					)
+# 	user.password = Faker::Internet.password
+# 	user.save
+# }
 
 
-10.times{
-	Api.create(title: Faker::Lorem.word,
-			   description: Faker::Lorem.sentence(3),
-			   tips: Faker::Lorem.sentence(3),
-			   average_score: rand(1.0..5.0),
-			   logo_url: "http://www.nasa.gov/sites/default/files/sydney.b.boen/images/twitter-small(1).gifv"
-			   )
-}
+# 10.times{
+# 	Api.create(title: Faker::Lorem.word,
+# 			   description: Faker::Lorem.sentence(3),
+# 			   tips: Faker::Lorem.sentence(3),
+# 			   average_score: rand(1.0..5.0),
+# 			   logo_url: "http://www.nasa.gov/sites/default/files/sydney.b.boen/images/twitter-small(1).gifv"
+# 			   )
+# }
 
-50.times {
-	review = Review.create(score: rand(1..5),
-				  		   content: Faker::Lorem.sentence(3),
-				           title: Faker::Lorem.word)
+# 50.times {
+# 	review = Review.create(score: rand(1..5),
+# 				  		   content: Faker::Lorem.sentence(3),
+# 				           title: Faker::Lorem.word)
 
-	api = Api.find(rand(1..10))
-	user = User.find(rand(1..20))
-	api.reviews << review
-	user.reviews << review
+# 	api = Api.find(rand(1..10))
+# 	user = User.find(rand(1..20))
+# 	api.reviews << review
+# 	user.reviews << review
 
-}
+# }
 
-100.times {
-	vote = Vote.create()
-	user = User.find(rand(1..20))
-	review = Review.find(rand(1..50))
-	user.votes << vote
-	review.votes << vote
-}
+# 100.times {
+# 	vote = Vote.create()
+# 	user = User.find(rand(1..20))
+# 	review = Review.find(rand(1..50))
+# 	user.votes << vote
+# 	review.votes << vote
+# }
 
-100.times {
-	comment = Comment.create(content: Faker::Lorem.sentence(3))
-	user = User.find(rand(1..20))
-	review = Review.find(rand(1..50))
-	user.comments << comment
-	review.comments << comment
-}
+# 100.times {
+# 	comment = Comment.create(content: Faker::Lorem.sentence(3))
+# 	user = User.find(rand(1..20))
+# 	review = Review.find(rand(1..50))
+# 	user.comments << comment
+# 	review.comments << comment
+# }
+
+require 'rest_client'
+require 'json'
+
+response = RestClient.get 'http://www.programmableweb.com/pw-api/views/query_apis', {:params => {'api-key' => 'NM2UJn3mhn2WgG9tLd3zTFG7sd8jdw9G', 'display_id' => 'api', 'name' => 'yelp', 'limit' => 1 }}
+
+json_response = Hash.from_xml(response).to_json
+
+puts json_response
+
+
