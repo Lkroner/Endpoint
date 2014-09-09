0d27b2dc92c6727fe3e0de90023b7639e003f200
Evan Simoni <evan.simoni@gmail.com>
2014-09-08 22:20:33 -0700

    adds successful query to retrieve api description and key_required

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -47,45 +47,28 @@
 
 require 'rest_client'
 require 'json'
-response = RestClient.get 'http://www.programmableweb.com/pw-api/views/query_apis', {:params => {'api-key' => 'NM2UJn3mhn2WgG9tLd3zTFG7sd8jdw9G', 'display_id' => 'api', 'keyword' => 'yelp', 'limit' => 1 }}
+response = RestClient.get 'http://www.programmableweb.com/pw-api/views/query_apis', {:params => {'api-key' => 'NM2UJn3mhn2WgG9tLd3zTFG7sd8jdw9G', 'display_id' => 'api', 'keyword' => 'yelp', 'limit' => 2 }}
 json_response = Hash.from_xml(response)
 
 # ***************  ATTRIBUTES  *********************
-title = json_response["result"]["item"]["title"]
-
+# title = json_response["result"]["item"][0]["title"]
+# description = json_response["result"]["item"][0]["field_api_description"]["und"]["item"]["value"]
+# key_required = json_response["result"]["item"][0]["field_api_developer_key_required"]["und"]["item"]["value"]
 # **************************************************
 
 puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-puts JSON.pretty_generate(json_response)
+# puts JSON.pretty_generate(json_response)
+puts json_response
 puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 
 
-puts json_response["result"]["item"]["title"]
+puts json_response["result"]["item"][0]["field_api_developer_key_required"]["und"]["item"]["value"]
+puts "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+puts json_response["result"]["item"][1]["field_api_developer_key_required"]["und"]["item"]["value"]
 
-# api_description = json_response["result"]
-# puts api_description
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++        
-# "field_api_description": {
-#                 "und": {
-#                     "is_array": "true",
-#                     "item": {
-#                         "format": null,
-#                         "safe_value": "Yahoo Maps API offers three ways to integrate maps into a website - a simple REST-based API for map images, an Ajax JavaScript API, and a Flash API. This summary refers to the AJAX API which is closest to Google and Microsoft models. \n\nThe Yahoo! AJAX Maps API lets developers add maps to their web sites using DHTML and JavaScript. Maps are fully embeddable and scriptable using the JavaScript programming language. Yahoo! Maps AS-Flash API&#039;s built-in geocoder means that developers can specify a physical address or latitude/longitude coordinates for a map&#039;s location, as needed. The Yahoo Map API can also provide for integrating geo-related information from Yahoo&#039;s products, such as local listings, traffic, weather, events, and photos. The functionality of the Maps API can be further extended to consume any other API.",
-#                         "value": "Yahoo Maps API offers three ways to integrate maps into a website - a simple REST-based API for map images, an Ajax JavaScript API, and a Flash API. This summary refers to the AJAX API which is closest to Google and Microsoft models. \n\nThe Yahoo! AJAX Maps API lets developers add maps to their web sites using DHTML and JavaScript. Maps are fully embeddable and scriptable using the JavaScript programming language. Yahoo! Maps AS-Flash API's built-in geocoder means that developers can specify a physical address or latitude/longitude coordinates for a map's location, as needed. The Yahoo Map API can also provide for integrating geo-related information from Yahoo's products, such as local listings, traffic, weather, events, and photos. The functionality of the Maps API can be further extended to consume any other API."
-#                     }
-#                 }
-#             }
-            
-# "field_api_developer_key_required": {
-#                 "und": {
-#                     "is_array": "true",
-#                     "item": {
-#                         "value": "Yes"
-#                     }
-#                 }
-#             }
-            
+
 #             "field_api_documentation": {
 #                 "und": {
 #                     "is_array": "true",
@@ -233,7 +216,6 @@ puts json_response["result"]["item"]["title"]
 #                 }
 #             }
 
-# "title": "Yahoo Maps"
 
             
 
