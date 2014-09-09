e03a6a27ff8b5f86ca9efdc49ebfc801ea2e121e
Nina Church <ninapearlchurch@gmail.com>
2014-09-08 20:28:31 -0700

    refactored navigation to API profile page

--- source/public/javascripts/views/search_results_view.js
+++ source/public/javascripts/views/search_results_view.js
@@ -17,6 +17,13 @@ ENDPOINT.Views.SearchResults = Backbone.View.extend({
       var searchView = new ENDPOINT.Views.SearchResult({model: searchModel});
       $("#search-list").append(searchView.render().$el);
     })
+    $(".result").on('click', this.navigateToProfile)
     return this;
+  },
+
+  navigateToProfile: function(){
+    event.preventDefault();
+    var id = this.dataset.id
+    ENDPOINT.router.navigate("api/"+id, true)
   }
 })
