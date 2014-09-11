b1f21fdcbec4c03e02469ad7be378ead335c1905
kbasma <katia.bazzi@gmail.com>
2014-09-10 17:08:43 -0700

    added category, doc links, key required info to api profile page

--- public/javascripts/views/search_results_view.js
+++ public/javascripts/views/search_results_view.js
@@ -2,7 +2,6 @@ ENDPOINT.Views.SearchResult = Backbone.View.extend({
   template: _.template($("#search-template").html()),
 
   render: function(){
-    debugger
     var filledTemplate = this.template(this.model.attributes);
     this.$el.html(filledTemplate)
     return this;
