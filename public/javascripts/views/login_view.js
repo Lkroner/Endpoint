c9aa809b5efe593eac2425aaa0fde8db64a8cb0f
kbasma <katia.bazzi@gmail.com>
2014-09-10 19:19:26 -0700

    Fixed login bug

--- public/javascripts/views/login_view.js
+++ public/javascripts/views/login_view.js
@@ -9,7 +9,7 @@ ENDPOINT.Views.LoginPage = Backbone.View.extend({
 	clickSubmit: function(){
 		this.email = $("input[name='email']").val();
 		this.password = $("input[name='password']").val();
-		this.model.fetch({email: this.email, password: this.password}).done(function(data){
+		this.model.fetch({data: {email: this.email, password: this.password}}).done(function(data){
 			if (data.user) {
 				$.cookie("user_id", data.user.id)
 				ENDPOINT.router.navigate("", true)
