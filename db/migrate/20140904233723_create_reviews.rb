ef477ed07d796a468053102c787396c26be9066f
kbasma <katia.bazzi@gmail.com>
2014-09-11 19:19:16 -0700

    allowing users to write long reviews

--- db/migrate/20140904233723_create_reviews.rb
+++ db/migrate/20140904233723_create_reviews.rb
@@ -2,8 +2,8 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
     	t.integer :score
-    	t.string :content
-    	t.string :title
+    	t.text :content
+    	t.text :title
     	t.belongs_to :api
     	t.belongs_to :user
       t.timestamps
